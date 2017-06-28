99a4726e12548030dc790033ae080ec254ca349e
James Wood <jamie_wood@ymail.com>
2017-06-28 12:50:39 +0100

    Begin implementing firebase database read and write

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+// Firebase config
+var config = {
+  apiKey: "AIzaSyBE5MOWlHjCG1uifUhViweuKo4pbn3RceY",
+  authDomain: "igda-survey-arcade.firebaseapp.com",
+  databaseURL: "https://igda-survey-arcade.firebaseio.com",
+  projectId: "igda-survey-arcade"
+};
+
+var database;
+
 document.getElementById("status").innerHTML = "starting up...";
 
 function showStatus()
@@ -6,6 +16,23 @@ function showStatus()
   {
     document.getElementById("status").innerHTML = "online";
     console.log("online");
+
+    // Initialize Firebase
+    firebase.initializeApp(config);
+
+    // Authenticate user
+    firebase.auth().signInWithEmailAndPassword("jamie.ct.wood@gmail.com", "m1n1flam3").catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+    });
+
+    // Get reference to the database
+    database = firebase.database();
+
+    // Read survey data
+    firebase.database().ref("surveys").once('value').then(function(snapshot) {
+      console.log(snapshot.val());
+    });
   }
   else
   {
