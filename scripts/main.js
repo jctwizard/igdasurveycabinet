b3bcf4b7dadcd4b057b711e9ba32de169e50420f
James Wood <jamie_wood@ymail.com>
2017-06-30 10:40:05 +0100

    Add editing, adding, removing

--- scripts/main.js
+++ scripts/main.js
@@ -59,23 +59,70 @@ function makeElement(parent, type, content, name, suffix)
   return newElement;
 }
 
+function getSurveyCount()
+{
+  return Object.keys(surveys).length;
+}
+
+function getQuestionCount(surveyIndex)
+{
+  return Object.keys(surveys["survey" + surveyIndex.toString()].questions).length;
+}
+
+function getAnswerCount(surveyIndex, questionIndex)
+{
+  return Object.keys(surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers).length;
+}
+
+function getSurveyName(surveyIndex)
+{
+  return surveys["survey" + surveyIndex.toString()].surveyName;
+}
+
+function getQuestionName(surveyIndex, questionIndex)
+{
+  return surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].questionName;
+}
+
+function getAnswerName(surveyIndex, questionIndex, answerIndex)
+{
+  return surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + answerIndex.toString()].answerName;
+}
+
+function getSurveys()
+{
+  return surveys;
+}
+
+function getQuestions(surveyIndex)
+{
+  return surveys["survey" + surveyIndex.toString()].questions;
+}
+
+function getAnswers(surveyIndex, questionIndex)
+{
+  return surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers;
+}
+
 function displaySurveys()
 {
   var editorPanel = document.getElementById("editorPanel");
-  var surveyNames = Object.keys(surveys);
 
   editorPanel.innerHTML = "";
 
   var surveyPanel = makeElement(editorPanel, "div", "", "surveyPanel", "");
 
-  for (var surveyIndex = 0; surveyIndex < surveyNames.length; surveyIndex++)
+  for (var surveyIndex = 0; surveyIndex < getSurveyCount(); surveyIndex++)
   {
     var surveyRow = makeElement(surveyPanel, "div", "", "surveyRow", surveyIndex.toString());
 
-    var surveyTitle = makeElement(surveyRow, "span", surveys[surveyNames[surveyIndex]].surveyName, "surveyTitle", surveyIndex.toString());
+    var surveyTitle = makeElement(surveyRow, "span", getSurveyName(surveyIndex), "surveyTitle", surveyIndex.toString());
 
     var surveyEditButton = makeElement(surveyRow, "button", "edit survey", "surveyEditButton", surveyIndex.toString());
     surveyEditButton.setAttribute("onclick", "editSurvey(" + surveyIndex.toString() + ")");
+
+    var surveyRemoveButton = makeElement(surveyRow, "button", "remove survey", "surveyRemoveButton", surveyIndex.toString());
+    surveyRemoveButton.setAttribute("onclick", "removeSurvey(" + surveyIndex.toString() + ")");
 
     var surveyRunButton = makeElement(surveyRow, "button", "run survey", "surveyRunButton", surveyIndex.toString());
     surveyRunButton.setAttribute("onclick", "runSurvey(" + surveyIndex.toString() + ")");
@@ -95,29 +142,26 @@ function editSurvey(surveyIndex)
   var editorPanel = document.getElementById("editorPanel");
   editorPanel.innerHTML = "";
 
-  var surveyNames = Object.keys(surveys);
-
-  var surveyHeader = makeElement(editorPanel, "input", surveys[surveyNames[surveyIndex]].surveyName, "surveyHeader", surveyIndex.toString());
+  var surveyHeader = makeElement(editorPanel, "input", getSurveyName(surveyIndex), "surveyHeader", surveyIndex.toString());
+  surveyHeader.setAttribute("onchange", "setSurveyName('" + surveyHeader.id + "', " + surveyIndex.toString() + ")");
 
   var questionPanel = makeElement(editorPanel, "div", "", "questionPanel", "")
 
-  var questionNames = Object.keys(surveys[surveyNames[surveyIndex]].questions);
-
-  for (var questionIndex = 0; questionIndex < questionNames.length; questionIndex++)
+  for (var questionIndex = 0; questionIndex < getQuestionCount(surveyIndex); questionIndex++)
   {
     var questionRow = makeElement(questionPanel, "div", "", "questionRow", questionIndex.toString());
 
-    var questionTitle = makeElement(questionRow, "span", surveys[surveyNames[surveyIndex]].questions[questionNames[questionIndex]].questionName, "questionTitle", questionIndex.toString());
+    var questionTitle = makeElement(questionRow, "span", getQuestionName(surveyIndex, questionIndex), "questionTitle", questionIndex.toString());
 
     var questionEditButton = makeElement(questionRow, "button", "edit question", "questionEditButton", questionIndex.toString());
     questionEditButton.setAttribute("onclick", "editQuestion(" + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
+
+    var questionRemoveButton = makeElement(questionRow, "button", "remove question", "questionRemoveButton", questionIndex.toString());
+    questionRemoveButton.setAttribute("onclick", "removeQuestion(" + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
   }
 
   var addQuestionButton = makeElement(editorPanel, "button", "add question", "addQuestionButton", "");
   addQuestionButton.setAttribute("onclick", "addQuestion(" + surveyIndex.toString() + ")");
-
-  var surveyCancelButton = makeElement(editorPanel, "button", "cancel edit", "surveyCancelButton", surveyIndex.toString());
-  surveyCancelButton.setAttribute("onclick", "displaySurveys()");
 
   var surveySaveButton = makeElement(editorPanel, "button", "save survey", "surveySaveButton", surveyIndex.toString());
   surveySaveButton.setAttribute("onclick", "saveSurvey(" + surveyIndex.toString() + ")");
@@ -131,88 +175,110 @@ function editQuestion(surveyIndex, questionIndex)
 
     editorPanel.innerHTML = "";
 
-    var surveyNames = Object.keys(surveys);
-    var questionNames = Object.keys(surveys[surveyNames[surveyIndex]].questions);
-
-    var questionHeader = makeElement(editorPanel, "input", surveys[surveyNames[surveyIndex]].questions[questionNames[questionIndex]].questionName, "questionHeader", questionIndex.toString());
+    var questionHeader = makeElement(editorPanel, "input", getQuestionName(surveyIndex, questionIndex), "questionHeader", questionIndex.toString());
+    questionHeader.setAttribute("onchange", "setQuestionName('" + questionHeader.id + "', " + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
 
     var answerPanel = makeElement(editorPanel, "div", "", "answerPanel", "")
 
-    var answerNames = Object.keys(surveys[surveyNames[surveyIndex]].questions[questionNames[questionIndex]].answers);
-
-    for (var answerIndex = 0; answerIndex < answerNames.length; answerIndex++)
+    for (var answerIndex = 0; answerIndex < getAnswerCount(surveyIndex, questionIndex); answerIndex++)
     {
       var answerRow = makeElement(answerPanel, "div", "", "answerRow", answerIndex.toString());
 
-      var answerTitle = makeElement(answerRow, "input", surveys[surveyNames[surveyIndex]].questions[questionNames[questionIndex]].answers[answerNames[answerIndex]].answerName, "answerTitle", answerIndex.toString());
+      var answerTitle = makeElement(answerRow, "input", getAnswerName(surveyIndex, questionIndex, answerIndex), "answerTitle", answerIndex.toString());
+      answerTitle.setAttribute("onchange", "setAnswerName('" + answerTitle.id.toString() + "', " + surveyIndex.toString() + ", " + questionIndex.toString() + ", " + answerIndex.toString() + ")");
+
+      var answerRemoveButton = makeElement(answerRow, "button", "remove answer", "answerRemoveButton", answerIndex.toString());
+      answerRemoveButton.setAttribute("onclick", "removeAnswer(" + surveyIndex.toString() + ", " + questionIndex.toString() + ", " + answerIndex.toString() + ")");
     }
 
     var addAnswerButton = makeElement(editorPanel, "button", "add answer", "addAnswerButton", "");
     addAnswerButton.setAttribute("onclick", "addAnswer(" + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
 
-    var questionCancelButton = makeElement(editorPanel, "button", "cancel edit", "questionCancelButton", questionIndex.toString());
-    questionCancelButton.setAttribute("onclick", "editSurvey(" + surveyIndex.toString() + ")");
-
     var questionSaveButton = makeElement(editorPanel, "button", "save question", "questionSaveButton", questionIndex.toString());
     questionSaveButton.setAttribute("onclick", "saveQuestion(" + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
 }
 
+function setSurveyName(elementId, surveyIndex)
+{
+  console.log(elementId);
+  console.log(document.getElementById(elementId).value);
+  surveys["survey" + surveyIndex.toString()].surveyName = document.getElementById(elementId).value;
+}
+
+function setQuestionName(elementId, surveyIndex, questionIndex)
+{
+  surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].questionName = document.getElementById(elementId).value;
+}
+
+function setAnswerName(elementId, surveyIndex, questionIndex, answerIndex)
+{
+  surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + answerIndex.toString()].answerName = document.getElementById(elementId).value;
+}
+
 function addSurvey()
 {
-  var surveyPanel = document.getElementById("surveyPanel");
-  var surveyNames = Object.keys(surveys);
-  var surveyIndex = surveyNames.length;
+  surveys["survey" + getSurveyCount().toString()] = { "surveyName":"new survey", "questions": {}};
 
-  var surveyRow = makeElement(surveyPanel, "div", "", "surveyRow", surveyIndex.toString());
-
-  var surveyTitle = makeElement(surveyRow, "span", "new survey", "surveyTitle", surveyIndex.toString());
-
-  var surveyEditButton = makeElement(surveyRow, "button", "edit survey", "surveyEditButton", surveyIndex.toString());
-  surveyEditButton.setAttribute("onclick", "editSurvey(" + surveyIndex.toString() + ")");
-
-  var surveyRunButton = makeElement(surveyRow, "button", "run survey", "surveyRunButton", surveyIndex.toString());
-  surveyRunButton.setAttribute("onclick", "runSurvey(" + surveyIndex.toString() + ")");
-
-  var surveyResultsButton = makeElement(surveyRow, "button", "view results", "surveyResultsButton", surveyIndex.toString());
-  surveyResultsButton.setAttribute("onclick", "viewSurveyResults(" + surveyIndex.toString() + ")");
-
-  surveys["survey" + surveyIndex.toString()] = { "surveyName":"new survey", "questions": {}};
-
-  editSurvey(surveyIndex);
+  displaySurveys();
 }
 
 function addQuestion(surveyIndex)
 {
-  var questionPanel = document.getElementById("questionPanel");
-  var surveyNames = Object.keys(surveys);
-  var questionNames = Object.keys(surveys[surveyNames[surveyIndex]]);
-  var questionIndex = questionNames.length;
+  surveys["survey" + surveyIndex.toString()].questions["question" + getQuestionCount(surveyIndex).toString()] = { "questionName":"new question", "answers": {}};
 
-  var questionRow = makeElement(questionPanel, "div", "", "questionRow", questionIndex.toString());
-
-  var questionTitle = makeElement(questionRow, "span", "new question", "questionTitle", questionIndex.toString());
-
-  var questionEditButton = makeElement(questionRow, "button", "edit question", "questionEditButton", questionIndex.toString());
-  questionEditButton.setAttribute("onclick", "editQuestion(" + surveyIndex.toString() + ", " + questionIndex.toString() + ")");
-
-  surveys[surveyNames[surveyIndex]].questions["question" + questionIndex.toString()] = { "questionName":"new question", "answers": {}};
-
-  editQuestion(surveyIndex, questionIndex);
+  editSurvey(surveyIndex);
 }
 
 function addAnswer(surveyIndex, questionIndex)
 {
-  var answerPanel = document.getElementById("answerPanel");
-  var surveyNames = Object.keys(surveys);
-  var questionNames = Object.keys(surveys[surveyNames[surveyIndex]]);
-  var answerNames = Object.keys(surveys[surveyNames[surveyIndex]][questionNames[questionIndex]]);
-  var answerIndex = answerNames.length;
+  surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + getAnswerCount(surveyIndex, questionIndex).toString()] = { "answerName":"enter answer here", "responses":0 };
 
-  var answerRow = makeElement(answerPanel, "div", "", "answerRow", answerIndex.toString());
+  editQuestion(surveyIndex, questionIndex);
+}
 
-  var answerTitle = makeElement(answerRow, "input", "enter answer here", "answerTitle", answerIndex.toString());
+function removeSurvey(surveyIndex)
+{
+  if (confirm("Are you sure you wish to remove this survey? The action cannot be undone and will delete the associated data."))
+  {
+    for (var otherSurveyIndex = surveyIndex; otherSurveyIndex < getSurveyCount() - 1; otherSurveyIndex++)
+    {
+      surveys["survey" + otherSurveyIndex.toString()] = surveys["survey" + (otherSurveyIndex + 1).toString()];
+    }
 
-  surveys[surveyNames[surveyIndex]].questions[questionNames[questionIndex]].answers["answer" + answerIndex.toString()] = { "answerName":"enter answer here", "responses":0 };
+    delete surveys["survey" + (getSurveyCount() - 1).toString()];
+  }
+
+  displaySurveys();
+}
+
+function removeQuestion(surveyIndex, questionIndex)
+{
+  if (confirm("Are you sure you wish to remove this question? The action cannot be undone and will delete the associated data."))
+  {
+    for (var otherQuestionIndex = questionIndex; otherQuestionIndex < getQuestionCount(surveyIndex) - 1; otherQuestionIndex++)
+    {
+      surveys["survey" + surveyIndex.toString()].questions["question" + otherQuestionIndex.toString()] = surveys["survey" + surveyIndex.toString()].questions["question" + (otherQuestionIndex + 1).toString()];
+    }
+
+    delete surveys["survey" + surveyIndex.toString()].questions["question" + (getQuestionCount(surveyIndex) - 1).toString()];
+  }
+
+  editSurvey(surveyIndex);
+}
+
+function removeAnswer(surveyIndex, questionIndex, answerIndex)
+{
+  if (confirm("Are you sure you wish to remove this answer? The action cannot be undone and will delete the associated data."))
+  {
+    for (var otherAnswerIndex = answerIndex; otherAnswerIndex < getAnswerCount(surveyIndex, questionIndex) - 1; otherAnswerIndex++)
+    {
+      surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + otherAnswerIndex.toString()] = surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + (otherAnswerIndex + 1).toString()];
+    }
+
+    delete surveys["survey" + surveyIndex.toString()].questions["question" + questionIndex.toString()].answers["answer" + (getAnswerCount(surveyIndex, questionIndex) - 1).toString()];
+  }
+
+  editQuestion(surveyIndex, questionIndex);
 }
 
 function saveSurvey(surveyIndex)
